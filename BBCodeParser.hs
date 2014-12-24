8002ce87335adf91502d0591e0e437c1f7a1b13e
gallais <guillaume.allais@ens-lyon.org>
2014-12-24 19:00:43 +0000

    [ new ] Tokenizer + Parser
    A better structured approach to parsing BBCode.

--- BBCodeParser.hs
+++ BBCodeParser.hs
@@ -1,15 +1,26 @@
-{-# OPTIONS  -Wall       #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS  -Wall             #-}
+{-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE ViewPatterns      #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
 
 module BBCodeParser where
 
 import Control.Applicative hiding (many, (<|>))
+import Data.Text (Text)
+import qualified Data.Text    as T
+import qualified Data.Text.IO as TIO
+
+import           BBCodeTokenizer (Token)
+import qualified BBCodeTokenizer as Tok
+
 import Text.Parsec
 
-newtype Name = Name { outName :: String }
+
+newtype Name = Name { outName :: Text }
  deriving Show
 
-newtype Url  = Url  { outUrl  :: String }
+newtype Url  = Url  { outUrl  :: Text }
  deriving Show
 
 data Tag = NULL | ID Name | CLASS Name
@@ -34,9 +45,87 @@ data Content =
   | FOOTNOTE     [Content]
   | URL      Url [Content]
   | IMG      Url
-  | RAW     String
+  | RAW     Text
  deriving Show
 
+finishesWith :: Stream s m Token => ParsecT s u m a -> Tok.OpClable -> ParsecT s u m a
+finishesWith p elt = p <* Tok.isExactly (Tok.CLOSETOK elt)
+
+withDigit :: Stream s m Token => (Text -> [a] -> [a]) -> ParsecT s u m [a] -> ParsecT s u m (Int, [a])
+withDigit emb p = Tok.isRaw >>= \ txt -> dig txt <$> p
+  where
+    dig txt xs = uncurry (\ pr sf -> (read $ T.unpack pr, emb sf xs)) $ T.breakOn "]" txt
+
+withTags :: Stream s m Token => (Text -> [a] -> [a]) -> ParsecT s u m [a] -> ParsecT s u m (Tag, [a])
+withTags emb p = Tok.isRaw >>= \ txt -> tag txt <$> p
+  where
+
+    getRest :: (Name -> Tag) -> Text -> (Tag, Text)
+    getRest c txt = uncurry (\ pr sf -> (c $ Name pr , sf)) $ T.breakOn "]" txt
+
+    tag (T.stripPrefix "id=" -> Just txt) xs = fmap (flip emb xs) $ getRest ID    txt
+    tag (T.stripPrefix "="   -> Just txt) xs = fmap (flip emb xs) $ getRest CLASS txt
+    tag (T.stripPrefix "]"   -> Just txt) xs = (NULL, emb txt xs)
+
+contents :: Stream s m Token => ParsecT s u m [Content]
+contents = many1 content
+
+-- smart constructor
+smart :: (Text -> a) -> Text -> [a] -> [a]
+smart emb txt xs
+  | T.null txt = xs
+  | otherwise  = emb txt : xs
+
+content :: Stream s m Token => ParsecT s u m Content
+content =
+  (do
+     switch <- Tok.isOneOf $ Tok.BR : fmap Tok.OPENTOK
+       [ Tok.IMG , Tok.I , Tok.B , Tok.U , Tok.TT , Tok.CENTER , Tok.FOOTNOTE
+       , Tok.URL , Tok.SPAN ]
+     case switch of
+       Tok.BR          -> pure BR
+       Tok.OPENTOK tok -> case tok of
+         Tok.IMG      -> IMG          <$> (Url <$> Tok.isRaw) `finishesWith` Tok.IMG
+         Tok.I        -> I            <$> contents            `finishesWith` Tok.I
+         Tok.B        -> B            <$> contents            `finishesWith` Tok.B
+         Tok.U        -> U            <$> contents            `finishesWith` Tok.U
+         Tok.TT       -> TT           <$> contents            `finishesWith` Tok.TT
+         Tok.CENTER   -> CENTER       <$> contents            `finishesWith` Tok.CENTER
+         Tok.FOOTNOTE -> FOOTNOTE     <$> contents            `finishesWith` Tok.FOOTNOTE
+         Tok.URL      -> undefined
+         Tok.SPAN     -> uncurry SPAN <$> withTags (smart RAW) contents `finishesWith` Tok.SPAN
+  ) <|> RAW . T.concat <$> many1 Tok.isRaw
+
+structures :: Stream s m Token => ParsecT s u m [Structure]
+structures = many1 structure
+
+structure :: Stream s m Token => ParsecT s u m Structure
+structure =
+  let txt = TXT . (:[]) . RAW in
+  (do
+    switch <- Tok.isOneOf $ fmap Tok.OPENTOK
+      [ Tok.DIV , Tok.H , Tok.P , Tok.UL ]
+    case switch of
+      Tok.OPENTOK tok -> case tok of
+        Tok.DIV -> uncurry DIV <$> withTags  (smart txt) (many structure `finishesWith` Tok.DIV)
+        Tok.H   -> uncurry H   <$> withDigit (smart RAW) (many content   `finishesWith` Tok.H)
+        Tok.P   -> uncurry P   <$> withTags  (smart txt) (many structure `finishesWith` Tok.P)
+        Tok.UL  -> undefined
+  ) <|> TXT <$> contents
+
+
+--
+--  (do
+--    switch <- choice $ fmap (try . string . ('[':))
+--              [ "div" , "h" , "p" , "ul" ]
+--    case tail switch of
+--      "div" -> uncurry DIV <$> withTags structures <* string "[/div]"
+--      "h"   -> H           <$> (read . (: []) <$> digit) <*> finishWith "[/h]" contents
+--      "p"   -> uncurry P   <$> withTags structures <* string "[/p]"
+--      "ul"  -> UL          <$> (char ']' *> ul)
+--  ) <|> TXT <$> contents
+
+{-
 url :: Stream s m Char => ParsecT s u m (Url, [Content])
 url = do
   link <- between (char '=') (char ']') $ many1 (try $ noneOf "]")
@@ -86,7 +175,7 @@ structure =
 contents :: Stream s m Char => ParsecT s u m [Content]
 contents = many1 content
 
-finishWith :: Stream s m Char => String -> ParsecT s u m a -> ParsecT s u m a
+finishWith :: Stream s m Char => Text -> ParsecT s u m a -> ParsecT s u m a
 finishWith str = between (char ']') (string str)
 
 escapedRaw :: Stream s m Char => ParsecT s u m Char
@@ -117,8 +206,9 @@ content =
 
 blogPost :: Stream s m Char => ParsecT s u m [Structure]
 blogPost = structure `manyTill` eof
+-}
 
 main :: FilePath -> IO ()
 main fp = do
-  input <- readFile fp
-  print $ parse blogPost "" input
+  input <- TIO.readFile fp
+  print $ parse (structure `manyTill` eof) "" $ Tok.tokenizer input
---
+++ BBCodeTokenizer.hs
@@ -0,0 +1,93 @@
+{-# OPTIONS  -Wall             #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE ViewPatterns      #-}
+
+module BBCodeTokenizer where
+
+import Data.Tuple
+import Data.List
+import Data.Text (Text)
+import qualified Data.Text as T
+
+import Control.Monad
+
+import Text.Parsec
+import Text.Parsec.Pos
+
+data OpClable =
+  P | H | DIV | SPAN | URL |
+  UL | U | I | B | TT | IMG | CENTER | FOOTNOTE
+  deriving (Eq, Show)
+
+data Token =
+    RAW      Text
+  | OPENTOK  OpClable
+  | CLOSETOK OpClable
+  | BR
+  deriving (Eq, Show)
+
+type Keywords = [(Token, Text)]
+
+keywords :: Keywords
+keywords =
+  let taggable tok tex = [ (OPENTOK tok, '['  `T.cons` tex)
+                         , (CLOSETOK tok, "[/" `T.append` tex `T.snoc` ']') ]
+      closable tok tex = [ (OPENTOK  tok, '[' `T.cons` tex `T.snoc` ']')
+                         , (CLOSETOK tok, "[/" `T.append` tex `T.snoc` ']') ]
+  in
+  (BR, "[br]")
+  :  concatMap (uncurry taggable)
+       [ (P, "p"), (H, "h"), (DIV, "div"), (SPAN, "span"), (URL, "url") ]
+  ++ concatMap (uncurry closable)
+       [ (UL, "ul"), (U, "u"), (I, "i"), (B, "b"), (TT , "tt"), (IMG, "img")
+       , (CENTER, "center"), (FOOTNOTE, "footnote") ]
+
+rawOrKeyword :: Keywords -> Text -> Maybe (Token, Text)
+rawOrKeyword ogkeys = go T.empty ogkeys
+  where
+    grabRest :: Char -> Keywords -> Keywords
+    grabRest c = fmap (fmap T.tail) . filter ((c ==) . T.head . snd)
+    go acc _ (T.uncons -> Nothing) = T.uncons acc >> return (RAW acc, T.empty)
+    go acc keys (T.uncons -> Just (c, t)) =
+      let matchedRest = grabRest c keys
+          matchedOG = grabRest c ogkeys in
+      if null matchedRest
+      then
+        if null matchedOG
+        then return (RAW (acc `T.snoc` c), t)
+        else return (RAW acc, c `T.cons` t)
+      else maybe
+            (go (acc `T.snoc` c) matchedRest t)
+            (\ tok -> return (tok, t))
+            (lookup T.empty $ fmap swap matchedRest)
+
+tokenize :: Text -> Maybe (Token, Text)
+tokenize = rawOrKeyword keywords
+
+tokenizer :: Text -> [Token]
+tokenizer = collapseRaws . unfoldr tokenize
+
+collapseRaws :: [Token] -> [Token]
+collapseRaws (RAW x : RAW y : xs) = collapseRaws $ RAW (x `T.append` y) : xs
+collapseRaws []     = []
+collapseRaws (x:xs) = x : collapseRaws xs
+
+updatePosToken :: SourcePos -> Token -> SourcePos
+updatePosToken sp = updatePosString sp . T.unpack . getText
+  where
+    getText (RAW txt) = txt
+    getText anything  = maybe "" id $ lookup anything keywords
+
+isExactly :: (Stream s m Token) => Token -> ParsecT s u m Token
+isExactly t = tokenPrim show (\ s -> const . (updatePosToken s))
+                             (\ u -> guard (t == u) >> Just t)
+
+isRaw :: (Stream s m Token) => ParsecT s u m Text
+isRaw = tokenPrim show (\ s -> const . (updatePosToken s)) fromRaw
+  where
+    fromRaw (RAW txt) = Just txt
+    fromRaw _         = Nothing
+
+isOneOf :: (Stream s m Token) => [Token] -> ParsecT s u m Token
+isOneOf = choice . fmap isExactly
