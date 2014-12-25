bc057117b8029d360e2e5bd36729bd694c6fccc2
gallais <guillaume.allais@ens-lyon.org>
2014-12-25 22:23:28 +0000

    [ more ] properly dealing with footnotes
    To properly deal with footnotes, we need to introduce the state monad
    giving us the ability to generate fresh names for references as well
    as storing the content until it is time to define the footer of the
    blog post.

--- Text/BBCode/PrettyPrinter.hs
+++ Text/BBCode/PrettyPrinter.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS  -Wall             #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
 
 module Text.BBCode.PrettyPrinter where
 
@@ -7,11 +8,12 @@ import Text.BBCode.Parser
 import Text.HTML.Combinators
 
 import Control.Applicative
-import Control.Monad.State
+import Control.Monad.State as CMS
 
 import Hakyll.Core.Item
 import Hakyll.Core.Compiler
 
+import Data.List
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy          as T
 import qualified Data.Text.Lazy.Encoding as TE
@@ -21,34 +23,38 @@ ppTag NULL         = ""
 ppTag (ID    name) = " id="    `T.append` outName name
 ppTag (CLASS name) = " class=" `T.append` outName name
 
-ppList :: (a -> Text) -> [a] -> Text
-ppList pp = T.concat . fmap pp
+ppList :: (Functor m, MonadState Footnotes m) => (a -> m Text) -> [a] -> m Text
+ppList pp = fmap T.concat . mapM pp
 
-ppContent :: Content -> Text
-ppContent BR              = br_
-ppContent (RAW      txt)  = txt
-ppContent (I        txts) = i_                $ ppList ppContent txts
-ppContent (B        txts) = b_                $ ppList ppContent txts
-ppContent (U        txts) = u_                $ ppList ppContent txts
-ppContent (TT       txts) = tt_               $ ppList ppContent txts
-ppContent (URL href txts) = a_ (outUrl href)  $ ppList ppContent txts
-ppContent (IMG      txt)  = img_              $ outUrl txt
-ppContent (SPAN tag txts) = span_ (ppTag tag) $ ppList ppContent txts
-ppContent (CENTER   txts) = center_           $ ppList ppContent txts
-ppContent (FOOTNOTE txts) = footnote_         $ ppList ppContent txts
+ppContent :: (Functor m, MonadState Footnotes m) => Content -> m Text
+ppContent BR              = return br_
+ppContent (RAW      txt)  = return txt
+ppContent (I        txts) = liftM i_                  $ ppList ppContent txts
+ppContent (B        txts) = liftM b_                  $ ppList ppContent txts
+ppContent (U        txts) = liftM u_                  $ ppList ppContent txts
+ppContent (TT       txts) = liftM tt_                 $ ppList ppContent txts
+ppContent (URL href txts) = liftM (a_ $ outUrl href)  $ ppList ppContent txts
+ppContent (IMG      txt)  = return $ img_             $ outUrl txt
+ppContent (SPAN tag txts) = liftM (span_ $ ppTag tag) $ ppList ppContent txts
+ppContent (CENTER   txts) = liftM center_             $ ppList ppContent txts
+ppContent (FOOTNOTE txts) = footnote_               =<< ppList ppContent txts
 
-ppStructure :: Structure -> Text
+ppStructure :: (Functor m, MonadState Footnotes m) => Structure -> m Text
 ppStructure (TXT cs)       = ppList ppContent cs
-ppStructure (DIV tag txts) = div_ (ppTag tag) $ ppList ppStructure txts
-ppStructure (H   int txts) = h_   int         $ ppList ppContent   txts
-ppStructure (P   tag txts) = p_   (ppTag tag) $ ppList ppStructure txts
-ppStructure (UL      lis)  = ul_              $ fmap (ppList ppContent) lis
+ppStructure (DIV tag txts) = liftM (div_ $ ppTag tag) $ ppList ppStructure txts
+ppStructure (H   int txts) = liftM (h_   int)         $ ppList ppContent   txts
+ppStructure (P   tag txts) = liftM (p_   $ ppTag tag) $ ppList ppStructure txts
+ppStructure (UL      lis)  = liftM ul_                $ mapM (ppList ppContent) lis
 
-ppBBCode :: [Structure] -> Text
-ppBBCode = T.intercalate "\n" . fmap ppStructure
+ppBBCode :: (Functor m, MonadState Footnotes m) => [Structure] -> m Text
+ppBBCode = fmap (T.intercalate "\n") . mapM ppStructure
 
 bbcodeCompiler :: Compiler (Item Text)
 bbcodeCompiler = do
   txt <- itemBody <$> getResourceLBS
-  let bbc = parseBBCode $ TE.decodeUtf8 txt
-  makeItem $ either (const "") ppBBCode bbc
+  let bbc       = parseBBCode $ TE.decodeUtf8 txt
+  let html      = either (return . T.pack . show) ppBBCode bbc
+  let (res, ft) = runState html (Footnotes 1 [])
+  makeItem $ T.concat $ res :
+    if null (list ft) then []
+    else h_ 3 "Footnotes" : intersperse br_ (reverse $ list ft)
--- Text/HTML/Combinators.hs
+++ Text/HTML/Combinators.hs
@@ -1,19 +1,36 @@
 {-# OPTIONS  -Wall             #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
 
 module Text.HTML.Combinators where
 
+import Control.Applicative
+import Control.Monad.State as CMS
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
+
+data Footnotes =
+  Footnotes { next :: Int
+            , list :: [Text] }
+
+push :: MonadState Footnotes m => Text -> m ()
+push str = CMS.modify $ \ (Footnotes n fs) -> Footnotes (n + 1) (str : fs)
 
 sandwichedWith :: Text -> Text -> Text
 str `sandwichedWith` bread = T.concat [ tag "" , str , tag "/" ]
   where tag elt = T.concat [ "<" , elt , bread , ">" ]
 
 a_ :: Text -> Text -> Text
-a_ href name =
+a_ href txt =
   T.concat [ "<a href=\"" , href , "\">"
-           , name
+           , txt
+           , "</a>" ]
+
+aWith_ :: Text -> Text -> Text -> Text
+aWith_ name href txt =
+  T.concat [ "<a id=\"" , name , "\""
+           , " href=\"" , href , "\">"
+           , txt
            , "</a>" ]
 
 br_ :: Text
@@ -47,8 +64,17 @@ img_ str = str `sandwichedWith` "img"
 center_ :: Text -> Text
 center_ str = str `sandwichedWith` "center"
 
-footnote_ :: Text -> Text
-footnote_ str = str `sandwichedWith` "footnote"
+
+footnote_ :: (Functor m, MonadState Footnotes m) => Text -> m Text
+footnote_ str = do
+  i <- T.pack . show <$> CMS.gets next
+  push   $ ref i "bot" "top" `T.append` T.cons ' ' str
+  return $ ref i "top" "bot"
+  where
+    ref num here there =
+      let anchorH = T.concat [ "ref" , here , num ]
+          anchorT = T.concat [ "#ref" , there, num ]
+      in aWith_ anchorH anchorT $ T.cons '[' $ num `T.snoc` ']'
 
 block_ :: Text -> Text -> Text -> Text
 block_ name tag str =
